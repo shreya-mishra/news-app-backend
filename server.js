e89a5449152ea4ecd403a686259ed2ab9854cbb9
SHREYA MISHRA <61110378+shreya-mishra@users.noreply.github.com>
2021-04-27 23:21:38 +0530

    added

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import newsRoutes from "./routes/newsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import viralRoutes from "./routes/viralRoutes.js";
 import magazineRoutes from "./routes/magazineRoutes.js";
+import viralRoutes from "./routes/viralRoutes.js";
+
 import { errorHandler, notFound } from "./middlewares/errorMiddlewares.js";
 import path from "path";
+
+
+let port = process.env.PORT || 7000
 
 const app = express();
 
@@ -17,17 +21,21 @@ app.use(express.json());
 const __dirname = path.resolve();
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 
+
 // connecting to db
 dotenv.config();
 connectDB();
 
 // Main API Routes
+
+app.use("/api", newsRoutes);
+
 app.use("/api/news", newsRoutes);
 app.use("/api/magazine", magazineRoutes);
 app.use("/api/viral", viralRoutes);
 app.use("/api/user", userRoutes);
 
-app.use("/api", (req, res) => {
+app.use("/", (req, res) => {
   res.send("API Working");
 });
 
@@ -35,7 +43,4 @@ app.use("/api", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(
-  5000,
-  console.log("Server Running on http://localhost:5000/api".yellow.bold)
-);
+app.listen(port, console.log(`Server Running on http://localhost:${port}/api`));
